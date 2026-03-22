7c9b6c88631fcb24b23c5d1eb8b4ff69a341a166
CodingInAVan <myounghoshin84@gmail.com>
2026-03-21 19:40:08 -0700

    Phase 1: Define backend interfaces and replace direct CUDA types in core APIs

---
+++ include/gpufl/core/backend_interfaces.hpp
@@ -0,0 +1,22 @@
+#pragma once
+
+#include <memory>
+#include <vector>
+
+#include "gpufl/core/events.hpp"
+#include "gpufl/core/sampler.hpp"
+
+namespace gpufl {
+
+class IGpuStaticInfoCollector {
+   public:
+    virtual ~IGpuStaticInfoCollector() = default;
+    virtual std::vector<CudaStaticDeviceInfo> sampleAll() = 0;
+};
+
+struct BackendCollectors {
+    std::shared_ptr<ISystemCollector<DeviceSample>> telemetry_collector;
+    std::unique_ptr<IGpuStaticInfoCollector> static_info_collector;
+};
+
+}  // namespace gpufl
